6def89eeff5321bf07579d91847c6a0ef89de7c1
hirokuma <uenokuma@gmail.com>
2013-01-13 12:21:51 +0900

    Static Handover NDEF作成(試作)

---
+++ HandoverWifi_FeliCaLite/HandoverWifi_FeliCaLite/HandoverWifi.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace HandoverWifi_FeliCaLite {
+	partial class HandoverWifi {
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent() {
+			this.textBefore = new System.Windows.Forms.TextBox();
+			this.buttonRead = new System.Windows.Forms.Button();
+			this.textMcBefore = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.buttonWrite = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textSsid = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textKey = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.comboAuth = new System.Windows.Forms.ComboBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.comboEnc = new System.Windows.Forms.ComboBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textMac = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBefore
+			// 
+			this.textBefore.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBefore.Location = new System.Drawing.Point(12, 24);
+			this.textBefore.Multiline = true;
+			this.textBefore.Name = "textBefore";
+			this.textBefore.ReadOnly = true;
+			this.textBefore.Size = new System.Drawing.Size(307, 180);
+			this.textBefore.TabIndex = 0;
+			// 
+			// buttonRead
+			// 
+			this.buttonRead.Location = new System.Drawing.Point(128, 260);
+			this.buttonRead.Name = "buttonRead";
+			this.buttonRead.Size = new System.Drawing.Size(75, 23);
+			this.buttonRead.TabIndex = 1;
+			this.buttonRead.Text = "Read";
+			this.buttonRead.UseVisualStyleBackColor = true;
+			this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+			// 
+			// textMcBefore
+			// 
+			this.textMcBefore.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textMcBefore.Location = new System.Drawing.Point(12, 235);
+			this.textMcBefore.Name = "textMcBefore";
+			this.textMcBefore.ReadOnly = true;
+			this.textMcBefore.Size = new System.Drawing.Size(307, 19);
+			this.textMcBefore.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 220);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(22, 12);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "MC";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(57, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "User Data";
+			// 
+			// buttonWrite
+			// 
+			this.buttonWrite.Enabled = false;
+			this.buttonWrite.Location = new System.Drawing.Point(411, 253);
+			this.buttonWrite.Name = "buttonWrite";
+			this.buttonWrite.Size = new System.Drawing.Size(75, 23);
+			this.buttonWrite.TabIndex = 1;
+			this.buttonWrite.Text = "Write";
+			this.buttonWrite.UseVisualStyleBackColor = true;
+			this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(349, 26);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(30, 12);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "SSID";
+			// 
+			// textSsid
+			// 
+			this.textSsid.Location = new System.Drawing.Point(385, 23);
+			this.textSsid.Name = "textSsid";
+			this.textSsid.Size = new System.Drawing.Size(122, 19);
+			this.textSsid.TabIndex = 7;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(354, 62);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(24, 12);
+			this.label7.TabIndex = 8;
+			this.label7.Text = "Key";
+			// 
+			// textKey
+			// 
+			this.textKey.Location = new System.Drawing.Point(385, 59);
+			this.textKey.Name = "textKey";
+			this.textKey.Size = new System.Drawing.Size(122, 19);
+			this.textKey.TabIndex = 9;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(349, 98);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(29, 12);
+			this.label8.TabIndex = 10;
+			this.label8.Text = "Auth";
+			// 
+			// comboAuth
+			// 
+			this.comboAuth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboAuth.FormattingEnabled = true;
+			this.comboAuth.Items.AddRange(new object[] {
+            "OPEN",
+            "WPA/PSK",
+            "SHARED",
+            "WPA",
+            "WPA2",
+            "WPA2/PSK"});
+			this.comboAuth.Location = new System.Drawing.Point(385, 95);
+			this.comboAuth.Name = "comboAuth";
+			this.comboAuth.Size = new System.Drawing.Size(122, 20);
+			this.comboAuth.TabIndex = 11;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(335, 135);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(44, 12);
+			this.label9.TabIndex = 12;
+			this.label9.Text = "Encrypt";
+			// 
+			// comboEnc
+			// 
+			this.comboEnc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboEnc.FormattingEnabled = true;
+			this.comboEnc.Items.AddRange(new object[] {
+            "NONE",
+            "WEP",
+            "TKIP",
+            "AES"});
+			this.comboEnc.Location = new System.Drawing.Point(385, 132);
+			this.comboEnc.Name = "comboEnc";
+			this.comboEnc.Size = new System.Drawing.Size(122, 20);
+			this.comboEnc.TabIndex = 13;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(348, 172);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(30, 12);
+			this.label10.TabIndex = 14;
+			this.label10.Text = "MAC";
+			// 
+			// textMac
+			// 
+			this.textMac.Location = new System.Drawing.Point(386, 169);
+			this.textMac.Name = "textMac";
+			this.textMac.Size = new System.Drawing.Size(122, 19);
+			this.textMac.TabIndex = 15;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(366, 204);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(120, 12);
+			this.label11.TabIndex = 16;
+			this.label11.Text = "※内容はチェックしません";
+			// 
+			// HandoverWifi
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(526, 296);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.textMac);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.comboEnc);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.comboAuth);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.textKey);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textSsid);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textMcBefore);
+			this.Controls.Add(this.buttonWrite);
+			this.Controls.Add(this.buttonRead);
+			this.Controls.Add(this.textBefore);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "HandoverWifi";
+			this.Text = "Wi-Fi Handover Card";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBefore;
+		private System.Windows.Forms.Button buttonRead;
+		private System.Windows.Forms.TextBox textMcBefore;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button buttonWrite;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textSsid;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textKey;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.ComboBox comboAuth;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.ComboBox comboEnc;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox textMac;
+		private System.Windows.Forms.Label label11;
+	}
+}
+
---
+++ HandoverWifi_FeliCaLite/HandoverWifi_FeliCaLite/HandoverWifi.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NfcStarterKitWrap;
+
+namespace HandoverWifi_FeliCaLite {
+	public partial class HandoverWifi : Form {
+		private nfc mFNS = new nfc();
+		private FelicaLite mLite = null;
+		private byte[] mWriteMc = null;
+		private byte[] mNfcId2 = new byte[nfc.NFCID2_SIZE];
+
+		public HandoverWifi() {
+			if(!mFNS.init(this)) {
+				MessageBox.Show("SDK for NFC Starter Kit fail");
+				Environment.Exit(0);
+				return;
+			}
+
+			InitializeComponent();
+
+			comboAuth.SelectedIndex = 0;
+			comboEnc.SelectedIndex = 0;
+			mLite = new NfcStarterKitWrap.FelicaLite(mFNS);
+		}
+
+		private void buttonRead_Click(object sender, EventArgs e) {
+			bool ret;
+
+			//書き込み不可
+			buttonWrite.Enabled = false;
+			textBefore.Text = "";
+
+			ret = prevProc();
+			if(!ret) {
+				afterProc();
+				MessageBox.Show("Check fail");
+				return;
+			}
+
+			StringBuilder sb = readUserBlock();
+
+			//最後まで成功
+			if(sb != null) {
+				textBefore.Text = sb.ToString();
+			}
+			else {
+				afterProc();
+				return;
+			}
+
+			sb = readMcBlock();
+			if(sb != null) {
+				textMcBefore.Text = sb.ToString();
+			}
+			else {
+				afterProc();
+				return;
+			}
+
+			afterProc();
+
+			//書き込み可能
+			buttonWrite.Enabled = true;
+		}
+
+		private void buttonWrite_Click(object sender, EventArgs e) {
+			bool ret;
+
+			//カードの存在チェック
+			ret = prevProc();
+			if(!ret) {
+				afterProc();
+				MessageBox.Show("Polling fail");
+				return;
+			}
+
+			//Readしたカードと同じかチェック
+			for(int i = 0; i < mNfcId2.Length; i++) {
+				if(mFNS.NfcId[i] != mNfcId2[i]) {
+					afterProc();
+					MessageBox.Show("Bad card");
+					return;
+				}
+			}
+
+			DialogResult dr = MessageBox.Show("Write ?", "confirm", MessageBoxButtons.YesNo);
+			if(dr == System.Windows.Forms.DialogResult.Yes) {
+				ret = writeCard(textSsid.Text, textKey.Text, comboAuth.SelectedIndex, comboEnc.SelectedIndex, textMac.Text);
+				if(ret) {
+					MessageBox.Show("Success!", "success");
+				}
+				else {
+					MessageBox.Show("Fail...", "fail");
+				}
+			}
+
+
+			afterProc();
+		}
+
+
+		/// <summary>
+		/// 事前処理
+		/// </summary>
+		/// <returns></returns>
+		private bool prevProc() {
+			//入力欄
+			if(textSsid.Text.Length == 0) {
+				return false;
+			}
+			if(textKey.Text.Length == 0) {
+				return false;
+			}
+			if(textMac.Text.Length != 12) {
+				return false;
+			}
+			for(int i = 0; i < 12; i += 2) {
+				try {
+					int b = Convert.ToByte(textMac.Text.Substring(i, 2), 16);
+				}
+				catch(Exception) {
+					return false;
+				}
+			}
+
+
+			bool ret = mFNS.pollingF();
+			if(ret) {
+				Buffer.BlockCopy(mFNS.NfcId, 0, mNfcId2, 0, mNfcId2.Length);
+			}
+			else {
+				Array.Clear(mNfcId2, 0, mNfcId2.Length);
+			}
+
+			return ret;
+		}
+
+		/// <summary>
+		/// 事後処理
+		/// </summary>
+		private void afterProc() {
+			mFNS.unpoll();
+
+			buttonWrite.Enabled = false;
+		}
+
+		private StringBuilder readUserBlock() {
+			bool ret = false;
+			byte[] rbuf;
+			StringBuilder sb = new StringBuilder((16 * 2 + 15 + 2) * 14);		//16byteとそのデリミタと\r\n
+
+			for(ushort blk = FelicaLite.BLOCK_PAD0; blk <= FelicaLite.BLOCK_PAD13; blk++) {
+				rbuf = null;
+				ret = mLite.Read(ref rbuf, blk);
+				if(ret) {
+					sb.Append(BitConverter.ToString(rbuf, 0));
+					sb.Append("\r\n");
+				}
+				else {
+					break;
+				}
+			}
+
+			if(!ret) {
+				sb = null;
+			}
+
+
+			return sb;
+		}
+
+		/// <summary>
+		/// MCブロックを読む。
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		private StringBuilder readMcBlock() {
+			bool ret = false;
+			StringBuilder sb = new StringBuilder(16 * 2 + 15);		//16byteとそのデリミタと\r\n
+
+			mWriteMc = null;
+			ret = mLite.Read(ref mWriteMc, FelicaLite.BLOCK_MC);
+			if(ret) {
+				sb.Append(BitConverter.ToString(mWriteMc, 0));
+				if(mWriteMc[3] == 0x01) {
+					//NDEFシステムコードだったら、MCブロックは書き込まない
+					mWriteMc = null;
+				}
+			}
+
+			return sb;
+		}
+
+
+		private bool writeCard(string ssid, string key, int auth, int enc, string mac) {
+			//Authentication Type:インデックス値→WPS値
+			byte auth_val;
+			switch(auth) {
+			case 0:	//OPEN
+				auth_val = 0x01;
+				break;
+			case 1:	//WPA/PSK
+				auth_val = 0x02;
+				break;
+			case 2:	//SHARED
+				auth_val = 0x04;
+				break;
+			case 3:	//WPA
+				auth_val = 0x08;
+				break;
+			case 4:	//WPA2
+				auth_val = 0x10;
+				break;
+			case 5:	//WPA2/PSK
+				auth_val = 0x20;
+				break;
+			default:
+				auth_val = 0x00;
+				break;
+			}
+
+			//Encryption Type:インデックス値→WPS値
+			byte enc_val;
+			switch(enc) {
+			case 0:	//NONE
+				enc_val = 0x01;
+				break;
+			case 1:	//WEP
+				enc_val = 0x02;
+				break;
+			case 2:	//TKIP
+				enc_val = 0x04;
+				break;
+			case 3:	//AES
+				enc_val = 0x08;
+				break;
+			default:
+				enc_val = 0x00;
+				break;
+			}
+	
+			NdefMessage msg = new NdefMessage();
+			NdefRecord rec_hs = new NdefRecord();	//Handover(static)
+			NdefRecord rec_ac = new NdefRecord();	//rec_hsのペイロードになる
+			NdefRecord rec_cr = new NdefRecord();	//Config Record
+
+			byte[] TYPE_HS = { (byte)'H', (byte)'s' };
+			byte[] TYPE_AC = { (byte)'a', (byte)'c' };
+			byte[] TYPE_CR_WIFI = {
+								0x61, 0x70, 0x70, 0x6c, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2f,
+								//a   p     p     l     i     c     a     t     i     o     n     /
+								0x76, 0x6e, 0x64, 0x2e, 0x77, 0x66, 0x61, 0x2e, 0x77, 0x73, 0x63,
+								//v   n     d     .     w     f     a     .     w     s     c
+							 };
+
+			rec_hs.MB = true;
+			rec_hs.ME = false;
+			rec_hs.setType(NdefRecord.TNF_TYPE.WKS, TYPE_HS);
+
+			rec_ac.MB = true;
+			rec_ac.ME = true;
+			rec_ac.setType(NdefRecord.TNF_TYPE.WKS, TYPE_AC);
+			rec_ac.Payload = new byte[] { 0x01, 0x01, (byte)'0', 0x00 };
+			byte[] rec_ac_byte = rec_ac.getRecord();
+
+			byte[] hs_pl = new byte[1 + rec_ac_byte.Length];
+			hs_pl[0] = 0x12;		//version
+			Buffer.BlockCopy(rec_ac_byte, 0, hs_pl, 1, rec_ac_byte.Length);
+			rec_hs.Payload = hs_pl;
+
+			rec_cr.MB = false;
+			rec_cr.ME = true;
+			rec_cr.setType(NdefRecord.TNF_TYPE.MIME, TYPE_CR_WIFI);
+			rec_cr.ID = new byte[] { (byte)'0' };
+
+			byte[] ssid_byte = System.Text.Encoding.UTF8.GetBytes(ssid);
+			byte[] key_byte = System.Text.Encoding.UTF8.GetBytes(key);
+			byte[] mac_byte = new byte[6];
+			for(int i = 0; i < 6; i++) {
+				mac_byte[i] = Convert.ToByte(mac.Substring(i * 2, 2), 16);
+			}
+			List<TLV> tlv_cred = new List<TLV> {
+				new TLV(0x1026, 0x0001, new byte[] { 0x01 }),
+				new TLV(0x1045, (ushort)ssid_byte.Length, ssid_byte),
+				new TLV(0x1003, 0x0002, new byte[] { 0x00, auth_val }),
+				new TLV(0x100f, 0x0002, new byte[] { 0x00, enc_val }),
+				new TLV(0x1027, (ushort)key.Length, key_byte),
+				new TLV(0x1020, 0x0006, mac_byte)
+			};
+			int tlv_cred_len = 0;
+			foreach(TLV tlv in tlv_cred) {
+				tlv_cred_len += tlv.getLength();
+			}
+			byte[] tlv_cred_byte = new byte[tlv_cred_len];
+			int pos = 0;
+			foreach(TLV tlv in tlv_cred) {
+				byte[] tlv_byte = tlv.get();
+				Buffer.BlockCopy(tlv_byte, 0, tlv_cred_byte, pos, tlv_byte.Length);
+				pos += tlv_byte.Length;
+			}
+
+			List<TLV> tlvs = new List<TLV> { 
+				new TLV(0x104a, 0x0001, new byte[] { 0x10 }),
+				new TLV(0x100e, (ushort)tlv_cred_byte.Length, tlv_cred_byte),
+				new TLV(0x1049, 0x0006, new byte[] { 0x00, 0x37, 0x2a, 0x00, 0x01, 0x20 })
+			};
+			int tlvs_len = 0;
+			foreach(TLV tlv in tlvs) {
+				tlvs_len += tlv.getLength();
+			}
+			byte[] tlvs_byte = new byte[tlvs_len];
+			pos = 0;
+			foreach(TLV tlv in tlvs) {
+				byte[] tlv_byte = tlv.get();
+				Buffer.BlockCopy(tlv_byte, 0, tlvs_byte, pos, tlv_byte.Length);
+				pos += tlv_byte.Length;
+			}
+			rec_cr.Payload = tlvs_byte;
+
+			msg.Add(rec_hs);
+			msg.Add(rec_cr);
+
+			byte[] msg_byte = msg.getMessage();
+
+			//Nexus7のバグなのか知らないが、113～127byteのNDEFデータは読めない
+			if((113 <= msg_byte.Length) && (msg_byte.Length <= 127)) {
+				MessageBox.Show("Nexus7 cannot Read this NDEF data.");
+			}
+
+			bool ret = false;
+
+			//ユーザブロック
+			int blocks = (msg_byte.Length + nfc.BLOCK_SIZE - 1) / nfc.BLOCK_SIZE;
+			for(ushort blk = 0; blk < blocks; blk++) {
+				ret = mLite.Write(msg_byte, (ushort)(1 + blk), 16 * blk);
+			}
+
+			//MCブロック
+			if(ret && (mWriteMc != null)) {
+				ret = mLite.Write(mWriteMc, FelicaLite.BLOCK_MC);
+			}
+
+			//Type3ヘッダ
+			if(ret) {
+				ret = writeType3Head(msg_byte.Length);
+			}
+
+			return ret;
+		}
+
+		private bool writeType3Head(int len) {
+			byte[] Type3Head = {
+								0x10,						//Ver		[0x00]
+								0x04,						//Nbr
+								0x01,						//Nbw
+								0x00, 0x0d,					//Nmaxb
+								0x00, 0x00, 0x00, 0x00,		//resv
+								0x00,						//WriteF
+								0x01,						//RW
+								0x00, 0x00, 0x00,			//Ln
+								0x00, 0x00					//ChkSum
+			};
+
+			Type3Head[0x0b] = (byte)((len >> 16) & 0xff);
+			Type3Head[0x0c] = (byte)((len >> 8) & 0xff);
+			Type3Head[0x0d] = (byte)(len & 0xff);
+			ushort sum = 0;
+			for(int i = 0; i < 14; i++) {
+				sum += (ushort)Type3Head[i];
+			}
+			Type3Head[0x0e] = (byte)((sum >> 8) & 0xff);	//ChkSum
+			Type3Head[0x0f] = (byte)(sum & 0xff);			//ChkSum
+
+			bool ret = mLite.Write(Type3Head, 0);
+			return ret;
+		}
+
+
+		class TLV {
+			private ushort mType;
+			private ushort mLen = 0;
+			private byte[] mValue = null;
+
+			public TLV() {
+			}
+
+			public TLV(ushort type, ushort len, byte[] val) {
+				set(type, len, val);
+			}
+
+			public void set(ushort type, ushort len, byte[] val) {
+				mType = type;
+				mLen = len;
+				if(mLen > 0) {
+					mValue = val;
+				}
+				else {
+					mValue = null;
+				}
+			}
+
+			public byte[] get() {
+				byte[] tlv = new byte[4 + mValue.Length];
+				tlv[0] = (byte)(mType >> 8);
+				tlv[1] = (byte)(mType & 0xff);
+				tlv[2] = (byte)(mLen >> 8);
+				tlv[3] = (byte)(mLen & 0xff);
+				if(mLen > 0) {
+					Buffer.BlockCopy(mValue, 0, tlv, 4, mValue.Length);
+				}
+
+				return tlv;
+			}
+
+			public int getLength() {
+				return mLen + 4;
+			}
+		}
+
+	}
+}
